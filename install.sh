c3f64c28ac0b7473c95d1ccdd0755fc90ba2e732
Adrian Băcîrcea <adrian.bacircea@gmail.com>
2015-01-10 13:35:05 +0200

    Factor linking to function

--- install.sh
+++ install.sh
@@ -16,45 +16,11 @@ source ${SCRIPTS_DIR}/base.source
 source ${SCRIPTS_DIR}/package_manager.source
 debug_on
 
-# symbolic links
-# SYMBOLIC_LINKS is an associative array
-#   key = file name
-#   value = where to link (supports multiple locations)
-# example:
-#   SYMBOLIC_LINKS+=(["file"]="location1 location2")
-#   SYMOBLIC_LINKS+=(["file"]=" location3")
-#   notice that the space is needed as the values will be appended
-#
-#   each folder from the install directory should have an install.source
-#   file that defines how the contents should be installed
-declare -A SYMBOLIC_LINKS
-
 for package in ${INSTALL_DIR}/*; do
     if [[ -f ${package}/install.source ]]; then
+        PACKAGE_DIR=${DOTFILES_DIR}/${package}
         source ${package}/install.source
     else
         debug_print "Warning (package ${package}): missing install.source"
     fi
 done
-
-for link in "${!SYMBOLIC_LINKS[@]}"; do
-    for destination in "${SYMBOLIC_LINKS[$link]}"; do
-        if [[ -e ${destination} ]]; then
-            echo "Link destination already exists."
-            answer=$(prompt_user \
-                "Are you sure you want to overwrite? (yes/no)"
-                "yes")
-            if [[ ${answer} = "yes" ]]; then
-                echo "Moving ${destination} to ${destination}.bak"
-                mv ${destination} ${destination}.bak
-            else
-                continue
-            fi
-        fi
-        debug_print "Linking ${DOTFILES_DIR}/$link to ${destination}"
-        mkdir -p $(dirname ${destination})
-        ln -s -f $DOTFILES_DIR/${link} ${destination}
-    done
-done
-
-
